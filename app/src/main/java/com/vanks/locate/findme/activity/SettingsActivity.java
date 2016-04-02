4e898be66c95a5467701107a413d9be9cfbee82c
Vaneyck Seme Otoki <vaneycksemeotoki@gmail.com>
2016-04-02 13:30:22 +0300

    Add SettingsActivity

---
+++ app/src/main/java/com/vanks/locate/findme/activity/SettingsActivity.java
@@ -0,0 +1,36 @@
+package com.vanks.locate.findme.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.vanks.locate.findme.R;
+
+/**
+ * Created by vaneyck on 02/04/2016.
+ */
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
